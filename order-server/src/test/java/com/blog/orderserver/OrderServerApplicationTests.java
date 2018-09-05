30636086909749df323579028359c0108e0626ff
徐冉 <xuran@xurandemacbook-air.local>
2018-09-05 17:40:21 +0800

    【重大更新】
        修复不能远程访问数据库问题

--- order-server/src/test/java/com/blog/orderserver/OrderServerApplicationTests.java
+++ order-server/src/test/java/com/blog/orderserver/OrderServerApplicationTests.java
@@ -1,16 +1,24 @@
 package com.blog.orderserver;
 
+import com.blog.orderserver.service.BlogService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(value = {"classpath:application.properties"})
 public class OrderServerApplicationTests {
+
+
+    @Autowired
+    BlogService blogService;
 
     @Test
     public void contextLoads() {
+        blogService.selectObjects();
+
     }
 
 }
